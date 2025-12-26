9371f20ed7c38e2db3165801d5e4b28fa139f953
bailey <bailey.crowe1599@gmail.com>
2025-12-25 19:56:25 -0600

    added updatecleanup

---
+++ shellscripts/updatecleanup.sh
@@ -0,0 +1,78 @@
+rm /usr/share/applications/Alacritty.desktop
+rm /usr/share/applications/Cemu.desktop
+rm /usr/share/applications/picom.desktop
+rm /usr/share/applications/xterm.desktop
+
+#rm /usr/share/applications/mirage.desktop
+rm /usr/share/applications/uxterm.desktop
+rm /usr/share/applications/electron32.desktop
+rm /usr/share/applications/electron34.desktop
+rm /usr/share/applications/thunar-settings.desktop
+rm /usr/share/applications/thunar-bulk-rename.desktop
+rm /usr/share/applications/dolphin-emu.desktop
+rm /usr/share/applications/libreoffice-base.desktop
+rm /usr/share/applications/libreoffice-draw.desktop
+rm /usr/share/applications/libreoffice-math.desktop
+rm /usr/share/applications/libreoffice-impress.desktop
+rm /usr/share/applications/libreoffice-startcenter.desktop
+rm /usr/share/applications/libreoffice-xsltfilter.desktop
+rm /usr/share/applications/tv.plex.PlexDesktop.desktop
+rm /usr/share/applications/rofi.desktop
+rm /usr/share/applications/rofi-theme-selector.desktop
+rm /usr/share/applications/org.flameshot.Flameshot.desktop
+rm /usr/share/applications/avahi-discover.desktop
+rm /usr/share/applications/bvnc.desktop
+rm /usr/share/applications/bssh.desktop
+rm /usr/share/applications/cmake-gui.desktop
+rm /usr/share/applications/electron27.desktop
+rm /usr/share/applications/lstopo.desktop
+cp /usr/share/applications/nm-connection-editor.desktop.bak /usr/share/applications/nm-connection-editor.desktop
+rm /usr/share/applications/org.kicad.pcbnew.desktop
+rm /usr/share/applications/org.kicad.eeschema.desktop
+rm /usr/share/applications/org.kicad.gerbview.desktop
+rm /usr/share/applications/org.kicad.pcbcalculator.desktop
+rm /usr/share/applications/org.kicad.bitmap2component.desktop
+rm /usr/share/applications/nvim.desktop
+rm /usr/share/applications/linguist.desktop
+rm /usr/share/applications/designer.desktop
+rm /usr/share/applications/qv4l2.desktop
+rm /usr/share/applications/assistant.desktop
+rm /usr/share/applications/qdbusviewer.desktop
+rm /usr/share/applications/qv4l2.desktop
+rm /usr/share/applications/qvidcap.desktop
+cp /usr/share/applications/heroic.desktop.bak /usr/share/applications/heroic.desktop
+rm /usr/share/applications/brave-browser.desktop
+rm /usr/share/applications/nemo.desktop
+cp /usr/share/applications/code-oss.desktop.bak /usr/share/applications/code-oss.desktop
+cp /usr/share/applications/org.keepassxc.KeePassXC.desktop.bak /usr/share/applications/org.keepassxc.KeePassXC.desktop
+cp /usr/share/applications/org.kicad.kicad.desktop.bak /usr/share/applications/org.kicad.kicad.desktop
+rm /usr/share/applications/steam.desktop
+rm /usr/share/applications/vesktop.desktop
+cp /usr/share/applications/gimp.desktop.bak /usr/share/applications/gimp.desktop
+cp /usr/share/applications/ryujinx.desktop.bak /usr/share/applications/ryujinx.desktop
+cp /usr/share/applications/spotify.desktop.bak /usr/share/applications/spotify-launcher.desktop
+cp /usr/share/applications/virt-manager.desktop.bak /usr/share/applications/virt-manager.desktop
+cp /usr/share/applications/vlc.desktop.bak /usr/share/applications/vlc.desktop
+rm /usr/share/applications/GPU\ Passthrough\ Manager.desktop
+rm /usr/share/applications/gparted.desktop
+cp /usr/share/applications/net.davidotek.pupgui2.desktop.bak /usr/share/applications/net.davidotek.pupgui2.desktop
+rm /usr/share/applications/assistant.desktop
+rm /usr/share/applications/qdbusviewer.desktop
+rm /usr/share/applications/linguist.desktop
+rm /usr/share/applications/designer.desktop
+cp /usr/share/applications/rpcs3.desktop.bak /usr/share/applications/rpcs3.desktop
+rm /usr/share/applications/renderdoc.desktop
+rm /usr/share/applications/btop.desktop
+cp /usr/share/applications/net.shadps4.shadPS4.desktop.bak net.shadps4.shadPS4.desktop
+cp /usr/share/applications/azahar.desktop.bak azahar.desktop
+rm /usr/share/applications/nwg-look.desktop
+rm /usr/share/applications/7zFM.desktop
+rm /usr/share/applications/calibre-ebook*
+mv /usr/share/applications/net.lutris.Lutris.desktop.bak /usr/share/applications/net.lutris.Lutris.desktop
+rm /usr/share/applications/jshell*
+rm /usr/share/applications/jconsole*
+rm /usr/share/applications/java*
+rm /usr/share/applications/uuctl.desktop
+rm /usr/share/applications/spotify-launcher.desktop
+rm /usr/share/applications/xgps.desktop
+rm /usr/share/applications/xgpsspeed.desktop
